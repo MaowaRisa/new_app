eeebc2fc595091f12df721702fd7124ed8dadcd7
maowarisa <maowarisa123@gmail.com>
2024-09-23 10:51:12 +1000

    Some change

--- App.js
+++ App.js
@@ -1,13 +1,68 @@
-const heading = React.createElement(
-    "div", 
-    {id: "parent"},
-    React.createElement("div", {id: "child"}, 
-        [
-            React.createElement("h1", {id: "heading"}, "I'm a H1 tag"),
-            React.createElement("h2", {}, "I'm a H2 tag")
-        ]
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+/**
+ * -Header
+ *  -Logo
+ *  -Nav Items
+ * -Body
+ *  -Search
+ *  -RestaurantContainer
+ *      -RestaurantCard
+ *          -Img
+ *          -Name, star rating, 
+ * -Footer
+ *  -Copyright
+ *  -Links
+ *  -Address
+ *  -Contact
+ */
+const Header = () =>{
+    return (
+        <div className="header">
+            <div className="logo-container">
+                <img className="logo" src="https://static.vecteezy.com/system/resources/previews/007/500/121/original/food-delivery-icon-clip-art-logo-simple-illustration-free-vector.jpg" alt="logo" />
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
     )
-)
+}
+const RestaurantCard = () =>{
+    return (
+        <div className="res-card">
+            <img src="https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg" alt="Image 1" />
+            <div>
+                Restaurant one
+            </div>
+
+        </div>
+    )
+}
+const Body = () => {
+    return (
+        <div className="body">
+            <div className="search">Search</div>
+            <div className="res-container">
+                <RestaurantCard/>
+            </div>
+        </div>
+    )
+}
+const AppLayout = () =>{
+    return (
+        <div className="app">
+            <Header/>
+            <Body/>
+        </div>
+    )
+}
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading)
+root.render(<AppLayout/>);
